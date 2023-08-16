5b0bfbaeb1af0822c926b25ff749d8bd3bd7a8d4
bday <brookeharris2248@gmail.com>
2023-08-16 17:34:05 -0400

    styling header still need to fix somethings, width correction

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import Header from './components/Header.jsx';
-//import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
